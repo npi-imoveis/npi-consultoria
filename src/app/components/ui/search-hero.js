309c40cf0885209337a766dc89ae25cea4e3407b
rkhael <renatokhael@gmail.com>
2025-04-09 20:39:56 -0300

    feat: ajuste search mobile

--- src/app/components/ui/search-hero.js
+++ src/app/components/ui/search-hero.js
@@ -17,10 +17,10 @@ export function SearchHero() {
   };
 
   return (
-    <div className="relative w-[350px] md:w-[600px] xl:w-[950px] bg-gray-100/10 rounded-full shadow-md p-1.5 transition-all duration-150 ease-in-out hover:scale-105 hover:shadow-lg">
-      <form onSubmit={handleSubmit}>
+    <div className="relative w-[300px] sm:w-[350px] md:w-[600px] xl:w-[950px] bg-gray-100/10 rounded-2xl sm:rounded-full shadow-md p-1.5 transition-all duration-150 ease-in-out hover:scale-105 hover:shadow-lg">
+      <form onSubmit={handleSubmit} className="flex flex-col sm:block gap-2 sm:gap-0">
         {/* Ícone de busca */}
-        <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+        <div className="absolute top-[1.15rem] sm:top-auto sm:inset-y-0 left-3 flex items-center pointer-events-none">
           <svg
             className="h-5 w-5 text-gray-400"
             xmlns="http://www.w3.org/2000/svg"
@@ -38,7 +38,7 @@ export function SearchHero() {
         {/* Campo de busca */}
         <input
           type="text"
-          className="font-semibold text-[10px] md:text-sm w-full pl-10 pr-24 py-2 md:py-3 text-white bg-transparent rounded-lg focus:outline-none placeholder-gray-300"
+          className="font-semibold text-[10px] md:text-sm w-full pl-10 pr-4 sm:pr-24 py-2 md:py-3 text-white bg-transparent rounded-lg focus:outline-none placeholder-gray-300"
           placeholder="Digite código, endereço, cidade ou condomínio..."
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
@@ -47,7 +47,7 @@ export function SearchHero() {
         {/* Botão de busca */}
         <button
           type="submit"
-          className="absolute text-xs md:text-sm right-1 top-1 bottom-1 px-4 md:px-6 bg-black/70 text-white font-bold rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#5044e4] flex items-center"
+          className="w-full sm:w-auto sm:absolute text-xs md:text-sm right-1 sm:top-1 sm:bottom-1 px-4 md:px-6 py-2 sm:py-0 bg-black/70 text-white font-bold rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#5044e4] flex items-center justify-center"
         >
           <span>Buscar imóveis</span>
         </button>
