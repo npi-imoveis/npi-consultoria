95528b978ea10456bcd52cc4b888cdd4f6d37ff1
rkhael <renatokhael@gmail.com>
2025-04-09 18:20:56 -0300

    feat: ajuste mapa

--- src/app/imovel-[id]/[slug]/componentes/ValoresUnidade.js
+++ src/app/imovel-[id]/[slug]/componentes/ValoresUnidade.js
@@ -21,19 +21,25 @@ export default function ValoresUnidade({ imovel }) {
         <div className="px-6 pt-6 bg-white rounded-lg">
             <div itemScope itemType="https://schema.org/Offer">
                 <p className="text-black font-medium">Preço:</p>
-                {imovel.Status == "LOCAÇÃO" ? (
-                    <h2 className="text-2xl font-bold mt-2" itemProp="price" content={imovel.ValorAluguelSite || "Consultar"}>
-                        {imovel.ValorAluguelSite !== "0" ? "R$ " + imovel.ValorAluguelSite : "Consultar"}
-                    </h2>
-                ) : (
-                    <h2 className="text-2xl font-bold mt-2" itemProp="price" content={imovel.ValorAntigo || "Consultar"}>
-                        {imovel.ValorAntigo !== "0" ? "R$ " + imovel.ValorAntigo : "Consultar"}
-                    </h2>
-                )}
+
+                <h2 className="text-2xl font-bold mt-2" itemProp="price" content={imovel.ValorAntigo || "Consultar"}>
+                    {imovel.ValorAntigo !== "0" ? "R$ " + imovel.ValorAntigo : "Consultar"}
+                </h2>
+
                 <meta itemProp="priceCurrency" content="BRL" />
             </div>
 
             <div className="grid grid-cols-2 gap-3 my-8">
+
+                {imovel.ValorAluguelSite && (
+                    <div className="flex flex-col bg-zinc-100 p-3 rounded-lg">
+                        <p className="text-sm text-zinc-600">Aluguel:</p>
+                        <p className="text-black font-semibold">
+                            R$  {imovel.ValorAluguelSite}
+                        </p>
+                    </div>
+                )}
+
                 <div className="flex flex-col bg-zinc-100 p-3 rounded-lg">
                     <p className="text-sm text-zinc-600">Condomínio</p>
                     <p className="text-black font-semibold">
