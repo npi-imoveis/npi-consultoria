b763bbd2a2d59eb7a24b85d25bef9856dd67a066
rkhael <renatokhael@gmail.com>
2025-04-17 16:37:50 -0300

    feat: add node cache

--- src/app/api/imoveis/route.js
+++ src/app/api/imoveis/route.js
@@ -1,7 +1,7 @@
 import { connectToDatabase } from "@/app/lib/mongodb";
 import Imovel, { IImovel } from "@/app/models/Imovel";
-
 import { NextResponse } from "next/server";
+import cache from "@/app/lib/cache";
 
 export async function GET(request) {
   try {
@@ -11,6 +11,15 @@ export async function GET(request) {
     const limit = parseInt(url.searchParams.get("limit") || "25", 10);
     const page = parseInt(url.searchParams.get("page") || "1", 10);
     const skip = (page - 1) * limit;
+
+    // Criar uma chave única para o cache baseada nos parâmetros
+    const cacheKey = `imoveis_page${page}_limit${limit}`;
+
+    // Verificar se os dados estão em cache
+    const cachedData = cache.get(cacheKey);
+    if (cachedData) {
+      return NextResponse.json(cachedData);
+    }
 
     await connectToDatabase();
 
@@ -28,7 +37,7 @@ export async function GET(request) {
     // Buscar imóveis com paginação e filtro
     const imoveis = await Imovel.find(filtro).skip(skip).limit(limit);
 
-    return NextResponse.json({
+    const response = {
       status: 200,
       data: imoveis,
       paginacao: {
@@ -37,7 +46,12 @@ export async function GET(request) {
         currentPage: page,
         limit,
       },
-    });
+    };
+
+    // Armazenar os dados em cache
+    cache.set(cacheKey, response);
+
+    return NextResponse.json(response);
   } catch (error) {
     console.error("Erro ao buscar imóveis:", error);
     return NextResponse.json(
