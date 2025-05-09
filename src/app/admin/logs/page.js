4f06f0e6fab9bdfc87ee50e4947144c50ca03548
rkhael <renatokhael@gmail.com>
2025-05-09 12:15:45 -0300

    ajuste: logs

--- src/app/admin/logs/page.js
+++ src/app/admin/logs/page.js
@@ -17,75 +17,88 @@ export default function Logs() {
 
   return (
     <AuthCheck>
-      <div className="relative overflow-x-auto">
-        <table className="min-w-full divide-y divide-gray-200">
-          <thead className="bg-gray-50">
-            <tr>
-              <th
-                scope="col"
-                className="px-6 py-3 text-left text-[10px] font-bold tracking-wider capitalize"
-              >
-                Usuário
-              </th>
-              <th
-                scope="col"
-                className="px-6 py-3 text-left text-[10px] font-bold tracking-wider capitalize"
-              >
-                E-mail
-              </th>
-              <th scope="col" className="px-6 py-3 text-left text-[10px] font-bold tracking-wider">
-                Data
-              </th>
-              <th scope="col" className="px-6 py-3 text-left text-[10px] font-bold tracking-wider">
-                Ação
-              </th>
-
-              <th
-                scope="col"
-                className="px-6 py-3 text-left text-[10px] font-bold  tracking-wider sticky right-0 bg-gray-50"
-              >
-                Ações
-              </th>
-            </tr>
-          </thead>
-          <tbody className="bg-white divide-y divide-gray-200">
-            {logs.map((log) => (
-              <tr key={log._id} className="hover:bg-gray-50">
-                <td className="px-6 bg-gray-50 py-4 whitespace-nowrap text-[10px] text-gray-900 font-bold capitalize">
-                  {log.user || "-"}
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap text-xs text-gray-900 font-bold capitalize">
-                  {log.email.toLowerCase() || "-"}
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap text-xs text-zinc-700">
-                  {log.data || "-"}
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap text-xs text-zinc-700">
-                  {log.action || "-"}
-                </td>
-
-                <td className="px-6 py-4 whitespace-nowrap sticky right-0 bg-white">
-                  <div className="flex items-center space-x-3">
-                    <button
-                      className="text-black font-bold hover:text-gray-700 bg-gray-100 p-2 rounded-md"
-                      title="Editar"
-                      onClick={() => handleEdit(corretor.codigoD)}
+      <div className="">
+        <div className="mb-8">
+          <h1 className="text-2xl font-bold text-gray-900 mb-4">Logs</h1>
+          <div className="relative overflow-x-auto mt-6">
+            <div className="h-[500px] overflow-y-auto rounded-lg">
+              <table className="min-w-full divide-y divide-gray-200 rounded-lg ">
+                <thead className="sticky top-0 z-10 bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-[10px] font-bold tracking-wider capitalize"
                     >
-                      <PencilSquareIcon className="h-5 w-5" />
-                    </button>
-                    <button
-                      className="text-red-500 font-bold hover:text-red-400 bg-gray-100 p-2 rounded-md"
-                      title="Deletar Imóvel"
-                      onClick={() => handleDelete(corretor.codigoD)}
+                      Usuário
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-[10px] font-bold tracking-wider capitalize"
                     >
-                      <TrashIcon className="h-4 w-4" />
-                    </button>
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+                      E-mail
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-[10px] font-bold tracking-wider"
+                    >
+                      Data
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-[10px] font-bold tracking-wider"
+                    >
+                      Ação
+                    </th>
+
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-[10px] font-bold  tracking-wider sticky right-0 bg-gray-50"
+                    >
+                      Ações
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200 h-[500px]">
+                  {logs.map((log) => (
+                    <tr key={log._id} className="hover:bg-gray-50">
+                      <td className="px-6 bg-gray-50 py-4 whitespace-nowrap text-[10px] text-gray-900 font-bold capitalize">
+                        {log.user || "-"}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-[10px] text-gray-900 font-bold capitalize">
+                        {log.email.toLowerCase() || "-"}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-[10px] text-zinc-700">
+                        {new Date(log.data).toLocaleString() || "-"}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-[10px] text-zinc-700">
+                        {log.action || "-"}
+                      </td>
+
+                      <td className="px-6 py-4 whitespace-nowrap sticky right-0 bg-white">
+                        <div className="flex items-center space-x-3">
+                          <button
+                            className="text-black font-bold hover:text-gray-700 bg-gray-100 p-2 rounded-md"
+                            title="Editar"
+                            onClick={() => handleEdit(corretor.codigoD)}
+                          >
+                            <PencilSquareIcon className="h-5 w-5" />
+                          </button>
+                          <button
+                            className="text-red-500 font-bold hover:text-red-400 bg-gray-100 p-2 rounded-md"
+                            title="Deletar Imóvel"
+                            onClick={() => handleDelete(corretor.codigoD)}
+                          >
+                            <TrashIcon className="h-4 w-4" />
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
     </AuthCheck>
   );
