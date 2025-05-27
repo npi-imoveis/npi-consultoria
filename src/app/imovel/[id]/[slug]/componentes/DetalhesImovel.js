8dd908acbb8ac7e41ca0ec6013a7da2fd187e9ec
NPI Imóveis <infranpiconsultoria@gmail.com>
2025-05-27 12:22:25 -0300

    Update DetalhesImovel.js

--- src/app/imovel/[id]/[slug]/componentes/DetalhesImovel.js
+++ src/app/imovel/[id]/[slug]/componentes/DetalhesImovel.js
@@ -3,18 +3,16 @@
 import { Bed, Bath, Car, ShowerHead } from "lucide-react";
 
 export default function DetalhesImovel({ imovel }) {
-  // Obtenha a metragem e certifique-se de que é uma string
-  let metragem = String(imovel.MetragemAnt || imovel.AreaPrivativa).trim();
+  // Garantir que a metragem seja uma string e remover possíveis espaços extras
+  const metragem = String(imovel.MetragemAnt || imovel.AreaPrivativa).trim();
 
-  // Se "m²" já estiver presente, não adicionamos novamente
-  if (!metragem.match(/\d+\s?m²$/)) {
-    metragem += " m²";
-  }
+  // Remover "m²" caso já esteja presente e adicionar corretamente depois
+  const metragemFormatada = metragem.endsWith("m²") ? metragem : `${metragem} m²`;
 
   return (
     <div className="flex flex-col bg-white container mx-auto border-t-2 p-4 md:p-10 mt-4">
       <span className="text-xl font-bold text-black">
-        {imovel.Categoria} de {metragem} para {imovel.Status.toLowerCase()}
+        {imovel.Categoria} de {metragemFormatada} para {imovel.Status.toLowerCase()}
       </span>
       <div className="flex flex-wrap gap-2 mt-8" aria-label="Características do imóvel">
         <div className="flex flex-col bg-zinc-100 rounded-lg items-center px-4 py-2 text-xs min-w-[110px]">
