b1e88f0ad1576fd5ef333ac5556624db1fa2c6ed
Renato Khael <renatokhael@gmail.com>
2025-06-05 19:16:57 -0300

    feat: ajuste api query

--- src/app/api/search/route.js
+++ src/app/api/search/route.js
@@ -1,51 +1,49 @@
 import { connectToDatabase } from "@/app/lib/mongodb";
-import ImovelAtivo from "@/app/models/ImovelAtivo";
+import Imovel from "@/app/models/Imovel";
 
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
-    try {
-        const { searchParams } = new URL(request.url);
-        const query = searchParams.get("q");
+  try {
+    const { searchParams } = new URL(request.url);
+    const query = searchParams.get("q");
 
-        if (!query || query.trim() === "") {
-            return NextResponse.json({
-                status: 200,
-                data: [],
-            });
-        }
-
-        await connectToDatabase();
-
-        // Utilizando o índice do Atlas Search com a consulta simplificada
-        const resultado = await ImovelAtivo.aggregate([
-            {
-                $search: {
-                    index: "default_1",
-                    text: {
-                        query: query,
-                        path: {
-                            wildcard: "*"
-                        }
-                    }
-                }
-            },
-            {
-                $limit: 20
-            },
-
-        ]);
-
-        return NextResponse.json({
-            status: 200,
-            data: resultado
-        });
-
-    } catch (error) {
-        console.error("Erro na busca:", error);
-        return NextResponse.json({
-            status: 500,
-            error: error.message || "Erro desconhecido"
-        });
+    if (!query || query.trim() === "") {
+      return NextResponse.json({
+        status: 200,
+        data: [],
+      });
     }
-} 
+
+    await connectToDatabase();
+
+    // Utilizando o índice do Atlas Search com a consulta simplificada
+    const resultado = await Imovel.aggregate([
+      {
+        $search: {
+          index: "imoveis",
+          text: {
+            query: query,
+            path: {
+              wildcard: "*",
+            },
+          },
+        },
+      },
+      {
+        $limit: 20,
+      },
+    ]);
+
+    return NextResponse.json({
+      status: 200,
+      data: resultado,
+    });
+  } catch (error) {
+    console.error("Erro na busca:", error);
+    return NextResponse.json({
+      status: 500,
+      error: error.message || "Erro desconhecido",
+    });
+  }
+}
