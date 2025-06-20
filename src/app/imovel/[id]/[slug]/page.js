d4743c5d0af3c79107e2a481cf00ba8b0fc25c52
NPI Imóveis <infranpiconsultoria@gmail.com>
2025-06-20 12:54:02 -0300

    Update page.js

--- src/app/imovel/[id]/[slug]/page.js
+++ src/app/imovel/[id]/[slug]/page.js
@@ -46,3 +46,75 @@ export async function generateMetadata({ params }) {
     },
     twitter: {
       card: "summary_large_image",
+      title,
+      description,
+      site: "@NPIImoveis",
+      images: destaqueFotoUrl ? [destaqueFotoUrl] : [],
+    },
+  };
+}
+
+export default async function Imovel({ params }) {
+  const { id, slug } = params;
+  const response = await getImovelById(id);
+
+  if (!response?.data) notFound();
+
+  const imovel = response.data;
+  const slugCorreto = imovel.Slug;
+
+  if (slug !== slugCorreto) {
+    redirect(`/imovel-${id}/${slugCorreto}`);
+  }
+
+  const currentUrl = `${process.env.NEXT_PUBLIC_SITE_URL}/imovel-${imovel.Codigo}/${imovel.Slug}`;
+
+  return (
+    <section className="w-full bg-white pb-32 pt-20">
+      <StructuredDataApartment
+        title={imovel.Empreendimento}
+        price={imovel.ValorAntigo ? `R$ ${imovel.ValorAntigo}` : "Consulte"}
+        description={`${imovel.Categoria} à venda em ${imovel.BairroComercial}, ${imovel.Cidade}. ${imovel.Empreendimento}: ${imovel.DormitoriosAntigo} quartos, ${imovel.Suites} suítes, ${imovel.BanheiroSocialQtd} banheiros, ${imovel.VagasAntigo} vagas, ${imovel.MetragemAnt}. ${imovel.Situacao}. Valor: ${imovel.ValorAntigo ? `R$ ${imovel.ValorAntigo}` : "Consulte"}. ${imovel.TipoEndereco} ${imovel.Endereco}.`}
+        address={`${imovel.TipoEndereco} ${imovel.Endereco}, ${imovel.Numero}, ${imovel.BairroComercial}, ${imovel.Cidade}`}
+        url={currentUrl}
+        image={imovel.Foto}
+      />
+
+      <ExitIntentModal condominio={imovel.Empreendimento} link={currentUrl} />
+      <div className="w-full mx-auto">
+        <ImageGallery imovel={imovel} />
+      </div>
+
+      <div className="container mx-auto gap-4 mt-3 px-4 md:px-0 flex flex-col lg:flex-row">
+        <div className="w-full lg:w-[65%]">
+          <TituloImovel imovel={imovel} currentUrl={currentUrl} />
+          <DetalhesImovel imovel={imovel} />
+          <DescricaoImovel imovel={imovel} />
+          <FichaTecnica imovel={imovel} />
+          <DetalhesCondominio imovel={imovel} />
+          <Lazer imovel={imovel} />
+          {imovel.Video && Object.keys(imovel.Video).length > 0 && (
+            <VideoCondominio imovel={imovel} />
+          )}
+          {imovel.Tour360 && (
+            <TourVirtual link={imovel.Tour360} titulo={imovel.Empreendimento} />
+          )}
+          <SimilarProperties id={imovel.Codigo} />
+          <LocalizacaoCondominio imovel={imovel} />
+        </div>
+
+        <div className="w-full lg:w-[35%] h-fit lg:sticky lg:top-24 order-first lg:order-last mb-6 lg:mb-0">
+          <Contato imovel={imovel} currentUrl={currentUrl} />
+        </div>
+      </div>
+
+      <div className="container mx-auto px-4 md:px-0">
+        <FAQImovel imovel={imovel} />
+      </div>
+
+      <WhatsappFloat
+        message={`Quero saber mais sobre o ${imovel.Empreendimento}, no bairro ${imovel.BairroComercial}, disponível na página do Imóvel: ${currentUrl}`}
+      />
+    </section>
+  );
+}
