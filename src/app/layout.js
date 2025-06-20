48c76ccf79f24a981973081e16c1cd1993177870
NPI Imóveis <infranpiconsultoria@gmail.com>
2025-06-20 12:52:02 -0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,6 @@ import { MusicPlayer } from "./components/shared/music-player";
 import { Organization, WebSite } from "./components/structured-data";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-
 import Script from "next/script";
 
 const oxanium = Oxanium({
@@ -26,30 +25,32 @@ const ANALYTICS_ID = "G-405E52JFGM";
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <body className={${oxanium.variable} ${michroma.variable} antialiased}>
+      <body className={`${oxanium.variable} ${michroma.variable} antialiased`}>
         <Script
+          id="gtm-script"
           dangerouslySetInnerHTML={{
-            __html: 
+            __html: `
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','${GTM_ID}');
-            ,
+            `,
           }}
         />
         <Script
-          src={https://www.googletagmanager.com/gtag/js?id=${ANALYTICS_ID}}
+          src={`https://www.googletagmanager.com/gtag/js?id=${ANALYTICS_ID}`}
           strategy="afterInteractive"
         />
         <Script id="gtag-init" strategy="afterInteractive">
-          {
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${ANALYTICS_ID}');
-        }
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${ANALYTICS_ID}');
+          `}
         </Script>
+
         <Organization />
         <WebSite />
         <QueryProvider>{children}</QueryProvider>
