b73bcdafa943bf55b889b1fb73c7e0dcd89119e7
NPI Imóveis <infranpiconsultoria@gmail.com>
2025-06-20 12:28:18 -0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,30 +26,29 @@ const ANALYTICS_ID = "G-405E52JFGM";
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <head /> {/* ← ESSENCIAL! Isso permite que <title> e <meta> sejam injetados no <head> */}
-      <body className={`${oxanium.variable} ${michroma.variable} antialiased`}>
+      <body className={${oxanium.variable} ${michroma.variable} antialiased}>
         <Script
           dangerouslySetInnerHTML={{
-            __html: `
+            __html: 
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','${GTM_ID}');
-            `,
+            ,
           }}
         />
         <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${ANALYTICS_ID}`}
+          src={https://www.googletagmanager.com/gtag/js?id=${ANALYTICS_ID}}
           strategy="afterInteractive"
         />
         <Script id="gtag-init" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${ANALYTICS_ID}');
-          `}
+          {
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${ANALYTICS_ID}');
+        }
         </Script>
         <Organization />
         <WebSite />
