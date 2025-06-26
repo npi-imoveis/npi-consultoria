f95ed35193f31d9cf22f06260b97cb9415ce3414
NPI Imóveis <infranpiconsultoria@gmail.com>
2025-06-26 19:01:01 -0300

    Update hero-section.js

--- src/app/components/sections/hero-section.js
+++ src/app/components/sections/hero-section.js
@@ -19,9 +19,10 @@ export function HeroSection() {
       <div className="absolute top-0 left-0 w-full h-full bg-black/80"></div>
 
       <div className="relative flex flex-col items-center justify-end h-full text-center text-white pb-24">
-        <h1 className="text-3xl md:text-5xl font-extrabold mb-4">
-          Imóveis de Alto Padrão
-        </h1>
+        <h1 className="text-white text-xl md:text-3xl font-semibold tracking-tight mb-2 md:mb-4">
+  Imóveis de Alto Padrão
+</h1>
+
 
         <Typewriter />
 
