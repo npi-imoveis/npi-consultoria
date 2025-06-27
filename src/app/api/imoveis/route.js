10de98e73ddcb427920fe11a3a657f67e6511854
NPI Imóveis <infranpiconsultoria@gmail.com>
2025-06-27 18:16:01 -0300

    Update route.js

--- src/app/api/imoveis/route.js
+++ src/app/api/imoveis/route.js
@@ -1,4 +1,3 @@
-export const dynamic = "force-dynamic";
 import { connectToDatabase } from "@/app/lib/mongodb";
 import Imovel from "@/app/models/Imovel";
 import { NextResponse } from "next/server";
