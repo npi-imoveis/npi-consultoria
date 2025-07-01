dfbff5e231b714ad7cd7941a7c061ea0b4385636
NPI Imóveis <infranpiconsultoria@gmail.com>
2025-07-01 15:21:27 -0300

    Update route.js

--- src/app/api/search/corretores/route.js
+++ src/app/api/search/corretores/route.js
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(request) {
   try {
-    const { searchParams } = new URL(request.url);
+    const { searchParams } = request.nextUrl;
     const query = searchParams.get("q");
 
     if (!query || query.trim() === "") {
