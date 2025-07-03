a35d51a94d28674cff19e86d1ec1df421441fe94
NPI Imóveis <infranpiconsultoria@gmail.com>
2025-07-02 21:46:20 -0300

    Update robots.js

--- src/app/robots.js
+++ src/app/robots.js
@@ -3,15 +3,21 @@
  * @returns {import('next').MetadataRoute.Robots}
  */
 export default function robots() {
-    const baseUrl = process.env.NEXT_PUBLIC_SITE_URL
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL;
 
-    return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-            disallow: ['/api/'],
-        },
-        host: baseUrl,
-        sitemap: `${baseUrl}/sitemap.xml`,
-    }
-} 
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/'],
+      },
+      {
+        userAgent: 'GPTBot',
+        allow: '/',
+      },
+    ],
+    host: baseUrl,
+    sitemap: `${baseUrl}/sitemap.xml`,
+  };
+}
