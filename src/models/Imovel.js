0593099a242bf9624085ea4270f41d2bd4a89f61
NPI Imóveis <infranpiconsultoria@gmail.com>
2025-07-04 20:38:59 -0300

    Update Imovel.js

--- src/models/Imovel.js
+++ src/models/Imovel.js
@@ -2,8 +2,7 @@ import mongoose from 'mongoose';
 
 const ImovelSchema = new mongoose.Schema({
   Codigo: { type: Number, required: true, unique: true },
-  Slug: { type: String, required: true },
-  // Adicione outros campos conforme necessário
+  Slug: { type: String, required: true }
 });
 
 export default mongoose.models.Imovel || mongoose.model('Imovel', ImovelSchema);
