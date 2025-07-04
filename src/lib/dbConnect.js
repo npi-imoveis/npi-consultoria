49ec9541b674a8f88b7ecbf7f4c453ff94cdfd58
NPI Imóveis <infranpiconsultoria@gmail.com>
2025-07-04 20:38:39 -0300

    Update dbConnect.js

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -2,25 +2,15 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-  throw new Error('Defina a variável MONGODB_URI no .env');
-}
+if (!MONGODB_URI) throw new Error('Defina a variável MONGODB_URI');
 
-let cached = global.mongoose;
+let cached = global.mongoose || { conn: null, promise: null };
 
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
-async function dbConnect() {
+export default async function dbConnect() {
   if (cached.conn) return cached.conn;
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      bufferCommands: false,
-    }).then(mongoose => mongoose);
+    cached.promise = mongoose.connect(MONGODB_URI).then(mongoose => mongoose);
   }
   cached.conn = await cached.promise;
   return cached.conn;
 }
-
-export default dbConnect;
