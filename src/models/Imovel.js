5d70b8a9040ab812cc5455309f674b3845c1cf79
NPI Imóveis <infranpiconsultoria@gmail.com>
2025-07-04 22:53:40 -0300

    Delete src/models/Imovel.js

--- src/models/Imovel.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const ImovelSchema = new mongoose.Schema({
-  Codigo: { type: Number, required: true, unique: true },
-  Slug: { type: String, required: true }
-});
-
-export default mongoose.models.Imovel || mongoose.model('Imovel', ImovelSchema);
