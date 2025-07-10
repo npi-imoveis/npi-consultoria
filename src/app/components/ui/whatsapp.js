7d9ea39d4d6bcc7c1b9d79057ae1309227827b06
NPI Imóveis <infranpiconsultoria@gmail.com>
2025-07-10 13:37:39 -0300

    Update whatsapp.js

--- src/app/components/ui/whatsapp.js
+++ src/app/components/ui/whatsapp.js
@@ -1,28 +1,22 @@
 "use client";
-
 import Image from "next/image";
 
 export function WhatsappFloat({ message }) {
   const handleWhatsAppClick = (e) => {
     e.preventDefault();
-
     // Função para detectar dispositivo móvel
     const isMobile = () => {
       return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
     };
-
     // Escolher a URL base apropriada
     const baseUrl = isMobile()
       ? 'whatsapp://send'
       : 'https://web.whatsapp.com/send';
-
     // Construir a URL completa
     let whatsappUrl = `${baseUrl}?phone=5511969152222`;
-
     if (message) {
       whatsappUrl = `${baseUrl}?phone=5511969152222&text=${encodeURIComponent(message)}`;
     }
-
     // Redirecionar para o WhatsApp
     if (isMobile()) {
       window.location.href = whatsappUrl;
@@ -32,21 +26,22 @@ export function WhatsappFloat({ message }) {
     }
   };
 
- return (
-  <div className="fixed bottom-5 right-5 z-[9999]">
-    
-      href="#"
-      onClick={handleWhatsAppClick}
-      className="cursor-pointer"
-    >
-      <Image 
-        src="/assets/images/whatsapp.png" 
-        height={48} 
-        width={48} 
-        alt="Fale conosco pelo WhatsApp" 
-        title="Fale conosco pelo WhatsApp - NPi Imóveis"
-        unoptimized 
-      />
-    </a>
-  </div>
-);
+  return (
+    <div className="fixed bottom-5 right-5 z-[9999]">
+      <a
+        href="#"
+        onClick={handleWhatsAppClick}
+        className="cursor-pointer"
+      >
+        <Image 
+          src="/assets/images/whatsapp.png" 
+          height={48} 
+          width={48} 
+          alt="Fale conosco pelo WhatsApp" 
+          title="Fale conosco pelo WhatsApp - NPi Imóveis"
+          unoptimized 
+        />
+      </a>
+    </div>
+  );
+}
