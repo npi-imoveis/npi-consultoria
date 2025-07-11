66a9c6f3b75e330dbf2bc2e07bee80b03e8acb90
NPI Imóveis <infranpiconsultoria@gmail.com>
2025-07-11 17:38:53 -0300

    Update robots.js

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,16 +1,21 @@
-
 /**
  * @returns {import('next').MetadataRoute.Robots}
  */
 export default function robots() {
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL;
-
   return {
     rules: [
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/api/'],
+        disallow: [
+          '/api/',
+          '/imovel-*/facebook.com/',
+          '/imovel-*/instagram.com/',
+          '/imovel-*/linkedin.com/',
+          '/imovel-*/twitter.com/',
+          '/imovel-*/youtube.com/'
+        ],
       },
       {
         userAgent: 'GPTBot',
