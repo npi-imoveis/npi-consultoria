668c45069590b37e6d6d1c842b214ebb74269b5d
NPI Imóveis <infranpiconsultoria@gmail.com>
2025-07-14 03:13:34 -0300

    Update luxury-grid-section.js

--- src/app/components/sections/luxury-grid-section.js
+++ src/app/components/sections/luxury-grid-section.js
@@ -9,7 +9,7 @@ export function LuxuryGridSection() {
           center
           section="Novidades"
           title="Outros produtos de luxo"
-          description="Em breve!"
+          description="Em breve em nosso HUB!"
         />
         <div className="w-full py-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-0 ">
           <CustomCard
