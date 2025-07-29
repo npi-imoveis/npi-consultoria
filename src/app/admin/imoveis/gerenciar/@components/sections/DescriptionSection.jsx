2cdc78a6cb6ea9b7b163aea9f0a00e272a2bc67d
NPI Imóveis <infranpiconsultoria@gmail.com>
2025-07-29 11:33:28 -0300

    Update DescriptionSection.jsx

--- src/app/admin/imoveis/gerenciar/@components/sections/DescriptionSection.jsx
+++ src/app/admin/imoveis/gerenciar/@components/sections/DescriptionSection.jsx
@@ -6,10 +6,12 @@ import FieldGroup from '../FieldGroup';
 
 const descriptionFields = [
   { name: "DescricaoUnidades", label: "Descrição da Unidade", type: "textarea" },
+  { name: "FichaTecnica", label: "Ficha Técnica", type: "textarea" },
   { name: "DescricaoDiferenciais", label: "Sobre o Condomínio", type: "textarea" },
+  { name: "DestaquesDiferenciais", label: "Destaques e Diferenciais", type: "textarea" },
   { name: "DestaquesLazer", label: "Destaques de Lazer", type: "textarea" },
   { name: "DestaquesLocalizacao", label: "Destaques de Localização", type: "textarea" },
-  { name: "FichaTecnica", label: "Ficha Técnica", type: "textarea" },
+  
 ];
 
 const DescriptionSection = ({ formData, displayValues, onChange }) => {
