50cb88120b81c4667d9b24fed9248e40bbc07ddf
NPI Imóveis <infranpiconsultoria@gmail.com>
2025-07-29 12:58:37 -0300

    Update page.js

--- src/app/(site)/[slug]/page.js
+++ src/app/(site)/[slug]/page.js
@@ -12,7 +12,6 @@ import { ImoveisRelacionados } from "./componentes/related-properties";
 import SobreCondominio from "./componentes/SobreCondominio";
 import FichaTecnica from "./componentes/FichaTecnica";
 import DiferenciaisCondominio from "./componentes/DiferenciaisCondominio";
-import DetalhesCondominio from "./componentes/DetalhesCondominio";
 import Lazer from "./componentes/Lazer";
 import VideoCondominio from "./componentes/VideoCondominio";
 import TourVirtual from "./componentes/TourVirtual";
