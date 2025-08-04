7514a8f12b8ffa39bac9fe1de8ba11fcf19becf5
NPI Imóveis <infranpiconsultoria@gmail.com>
2025-08-04 19:26:17 -0300

    Update related-properties.js

--- src/app/(site)/[slug]/componentes/related-properties.js
+++ src/app/(site)/[slug]/componentes/related-properties.js
@@ -1,26 +1,24 @@
 "use client";
-
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
 import { useRef } from "react";
 import CardImovel from "@/app/components/ui/card-imovel";
 
 export function ImoveisRelacionados({ imoveisRelacionados }) {
   const carouselRef = useRef(null);
-
+  
   const scroll = (direction) => {
     if (carouselRef.current) {
       const scrollAmount = 300;
       carouselRef.current.scrollLeft += direction === "left" ? -scrollAmount : scrollAmount;
     }
   };
-
+  
   return (
     <section className="relative bg-white container mx-auto p-10 mt-4 rounded-lg">
       <div className="flex justify-between items-center mb-6">
         <h2 className="text-xl font-bold text-black">Imóveis no mesmo condomínio</h2>
         <span className="text-xs text-zinc-700 font-semibold">Encontrado {imoveisRelacionados.length} {imoveisRelacionados.length === 1 ? "imóvel" : "imóveis"}</span>
       </div>
-
       <div className="container mx-auto">
         <button
           onClick={() => scroll("left")}
@@ -28,7 +26,6 @@ export function ImoveisRelacionados({ imoveisRelacionados }) {
         >
           <ChevronLeftIcon className="w-6 h-6" />
         </button>
-
         <div
           ref={carouselRef}
           className="flex gap-4 overflow-x-auto scroll-smooth scrollbar-hide no-scrollbar pb-4"
@@ -37,14 +34,12 @@ export function ImoveisRelacionados({ imoveisRelacionados }) {
             <p className="text-center w-full py-8">Nenhum imóvel relacionado encontrado.</p>
           ) : (
             imoveisRelacionados.map((imovel) => (
-
               <div key={imovel.Codigo || imovel._id || `imovel-${Math.random()}`} className="min-w-[320px]">
-                <CardImovel {...imovel} isLoading={false} />
+                <CardImovel {...imovel} isLoading={false} target="_blank" />
               </div>
             ))
           )}
         </div>
-
         <button
           onClick={() => scroll("right")}
           className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded-full shadow-md z-10"
