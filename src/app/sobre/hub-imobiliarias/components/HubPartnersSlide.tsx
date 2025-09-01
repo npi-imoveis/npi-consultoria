2c6c1899a337eaf3f80e0e9733bcdf7ae5e44b38
NPI Imóveis <infranpiconsultoria@gmail.com>
2025-09-01 18:10:26 -0300

    Update HubPartnersSlide.tsx

--- src/app/sobre/hub-imobiliarias/components/HubPartnersSlide.tsx
+++ src/app/sobre/hub-imobiliarias/components/HubPartnersSlide.tsx
@@ -30,9 +30,11 @@ const hubLogos = [
   "/assets/images/hub-partners/andrea-gomes.jpg",
   "/assets/images/hub-partners/prime-one.webp",
   "/assets/images/hub-partners/logo-npi-degrade-transparente-oficial.png",
-  "/assets/images/hub-partners/verano.png",
-  "/assets/images/hub-partners/immobile-unique.png",
+  "/assets/images/hub-partners/verano.jpg",
+  "/assets/images/hub-partners/immobile-unique.jpg",
   "/assets/images/hub-partners/robles.png",
+  "/assets/images/hub-partners/Eurico.png",
+  "/assets/images/hub-partners/remax-trio.jpeg",
   // Adicione quantos logos precisar
 ];
 
