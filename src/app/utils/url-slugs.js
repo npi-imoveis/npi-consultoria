4ec4fe7ebfbbcf432bcbd7c25d84a9d73823be7b
NPI Imóveis <infranpiconsultoria@gmail.com>
2025-09-18 12:15:57 -0300

    Update url-slugs.js

--- src/app/utils/url-slugs.js
+++ src/app/utils/url-slugs.js
@@ -32,7 +32,6 @@ export const getCitySlugMapping = async () => {
 // Fallback mapping for when API is not available
 const getFallbackCityMapping = () => {
   return {
-    'sao-paulo': 'São Paulo',
     'arraial-dajuda-porto-seguro': 'Arraial DAjuda (Porto Seguro)',
     'atibaia': 'Atibaia',
     'balneario-camboriu': 'Balneário Camboriú',
